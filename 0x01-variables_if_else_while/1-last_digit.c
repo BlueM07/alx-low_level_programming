fce7536c3145420d3f690633e5f5041ba0798a20
Mariam <mariamernissi@gmail.com>
2023-03-18 15:04:06 -0700

    r

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastdigit = n%10;
 if (lastdigit>5) {
-printf("last digit of %d is %d and is greater than 5" , n,lastdigit);
+printf("Last digit of %d is %d and is greater than 5" , n,lastdigit);
 }
 else
 {
 if (lastdigit<6 && lastdigit!=0) {
-printf ("last digit of %d is %d and is less than 6 and not 0",n , lastdigit);
+printf ("Last digit of %d is %d and is less than 6 and not 0",n , lastdigit);
 }
 else {
-printf ("last digit of %d is %d and is 0" , n,lastdigit);
+printf ("Last digit of %d is %d and is 0" , n,lastdigit);
 }
 }
 return (0);
