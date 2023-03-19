924885fe1d0d1bce33800441d17b22fa6c909e3c
Mariam <mariamernissi@gmail.com>
2023-03-19 08:02:42 -0700

    r

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,12 @@
 */
 int main(void)
 {
-char s1;
-char s2;
-char s3;
-for (s1 = 'a' && s2 = 'f' && s3 = 'r' ; s1 <= 'z' && s2 <= 'z' && s3 <= 'z' && s1 != 'e' && s1 != 'q' ; s1++ && s2++ & s3++)
+char a;
+for (a = 'a'; a <= 'z' && a != 'e'; a++)
+for (a = 'f'; a <= 'z' && a != 'q' ; a++)
+for (a = 'r'; a <= 'z'; a++)
 {
-putchar(s1,s2,s3);
+putchar(a);
 }
 putchar('\n');
 return (0);
