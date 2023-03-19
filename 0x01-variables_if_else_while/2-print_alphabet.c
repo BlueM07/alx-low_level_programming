0a88d9637aabc7baa17bf0d8083a9f9463719219
Mariam <mariamernissi@gmail.com>
2023-03-19 06:29:08 -0700

    r

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - gives alphabet
+* a is alphabet
+*
+* main function produces the whole alphabet in lowercase
+* description header
+* return the alphabet
+*/
+int main (void)
+{
+char a;
+char a = 'a';
+putchar(a\n);
+return (a)
+}
