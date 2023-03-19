2549eebe416b51acbf8054917956de79d3cb0fe9
Mariam <mariamernissi@gmail.com>
2023-03-19 09:43:30 -0700

    r

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - gives alphabet
+* a is alphabet
+(*
+* main function produces the whole alphabet in lowercase
+(* description header)
+* Return: value of main is the alphabet
+*/
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n = n + 1)
+{
+putchar(n);
+putchar (", ")
+}
+putchar('\n');
+return (0);
+}
