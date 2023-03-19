5157f7065ed49f637c5e3600a6a356e5eed24b6d
Mariam <mariamernissi@gmail.com>
2023-03-19 08:53:55 -0700

    r

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - gives alphabet
+* a is alphabet
+(*
+* main function produces the whole alphabet in lowercase
+(* description header)
+* Return: value of main is the alphabet
+*/
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n = n+1)
+{
+putchar(n);
+}
+return (0);
+}
