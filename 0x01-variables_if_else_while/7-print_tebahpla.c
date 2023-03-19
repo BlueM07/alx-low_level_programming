db33afc5a7777420dd309728c054d7a965b2acaa
Mariam <mariamernissi@gmail.com>
2023-03-19 09:01:05 -0700

    r

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - gives alphabet
+* a is alphabet
+(*
+* main function produces the whole alphabet in lowercase
+(* description header)
+* Return: value of main is the alphabet
+*/
+int main(void)
+{
+int n;
+for (n = '10'; n >= '0'; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
