edd7ba9bfb6ab4189d67f27cb340b35fe8f61367
Mariam <mariamernissi@gmail.com>
2023-03-20 15:52:35 -0700

    r

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
 */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\n");
-printf("Size of an int: 4 byte(s)\n");
-printf("Size of a long int: 4 byte(s)\n");
-printf("Size of a long long int: 8 byte(s)\n");
-printf("Size of a float: 4 byte(s)\n");
+printf("Size of a char: %u byte(s)\n");
+printf("Size of an int: %u byte(s)\n");
+printf("Size of a long int: %u byte(s)\n");
+printf("Size of a long long int: %u byte(s)\n");
+printf("Size of a float: %u byte(s)\n");
 return (0);
 }
