61e92cc700ccc75757f7d778c65673fa999c0d6a
Mariam <mariamernissi@gmail.com>
2023-03-20 15:04:16 -0700

    r

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - puts a string
+* parameter
+(*
+* Description: prints a specific string
+(* section header)
+* Return: the demanded string
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
