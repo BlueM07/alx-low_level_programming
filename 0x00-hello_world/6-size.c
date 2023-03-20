1a0fe3acd06e9613276a9b4068da1e45606c70a2
Mariam <mariamernissi@gmail.com>
2023-03-20 15:21:33 -0700

    r

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - puts a string
+* parameter
+(*
+* Description: prints a specific string
+(* section header)
+* Return: the demanded string
+*/
+int main(void)
+{
+printf("%lu/n", sizeof(char));
+printf("%lu/n", sizeof(int));
+printf("%lu/n", sizeof(long int));
+printf("%lu/n", sizeof(long long int));
+printf("%lu/n", sizeof(float));
+return (0);
+}
