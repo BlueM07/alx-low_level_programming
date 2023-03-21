59bd5a534e35516523bf05b45e0049f62f734d9d
Mariam <mariamernissi@gmail.com>
2023-03-21 12:35:58 -0700

    r

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <main.h>
-/**
- * name - print _putchar
- * parameter
-(*
-* Description: prints a specific string
-(* section header)
-* Return: the demanded string
-*/
 int main(void)
 {
 	char ptch = "_putchar";
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * name - print _putchar
+ * parameter
+(*
+* Description: prints a specific string
+(* section header)
+* Return: the demanded string
+*/
+int main(void)
+{
+return (0);
+}
