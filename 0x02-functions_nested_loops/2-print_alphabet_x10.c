5d2a3226febc4919bf2457032b6f30cd146a259a
Mariam <mariamernissi@gmail.com>
2023-03-22 15:58:56 -0700

    r

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print _alphabet
+ * parameter
+(*
+* Description: prints a specific string
+(* section header)
+* Return: the demanded string
+*/
+void print_alphabet_x10(void)
+{
+        char a;
+	int n;
+
+	for (n = 0; n <= 10; n = n + 1)
+	{
+	print_alphabet();
+	}
+}
