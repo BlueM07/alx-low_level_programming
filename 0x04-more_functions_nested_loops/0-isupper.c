19d87b573a3b347e85d4c27c9706a530bce2dce2
Mariam <mariamernissi@gmail.com>
2023-03-23 14:52:19 -0700

    r

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * isupper - checks for uppercase character
+ * parameter
+(*
+* Description: prints a specific string
+(* section header)
+* Return: the demanded string
+*/
+int _isupper(int c);
+{
+int c;
+int i;
+i = isupper(c);
+c = 'A';
+_putchar(i);
+c = 'a';
+_putchar(i);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * name - print _putchar
+ * parameter
+(*
+* Description: prints a specific string
+(* section header)
+* Return: the demanded string
+*/
+int main(void);
+char _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isupper(int c);
