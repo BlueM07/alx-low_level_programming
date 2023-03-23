05be468270dcc841a7284f30547a3fbeb89d799c
Mariam <mariamernissi@gmail.com>
2023-03-23 10:39:41 -0700

    r

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,15 +3,11 @@
  * main - tests a number
  * Return: 0
  * */
-
-
 int main (void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+i = 0;
+positive_or_negative(i);
+return (0);
 }
-
