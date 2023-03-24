7d392d0342abfef406e2ecc16d701376e20a2946
Mariam <mariamernissi@gmail.com>
2023-03-24 16:02:16 -0700

    r

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_most_numbers - gives numbers
+* 
+* Description: gives numbers from 0 to 9 except 2 and 4
+* Return: value of main is the alphabet
+*/
+void print_most_numbers(void)
+{
+int n;
+for (n = '0'; n <= '9'; n = n + 1)
+{
+if (n != '2' && n != '4')
+{
+putchar(n);
+}
+}
+putchar('\n');
+}
