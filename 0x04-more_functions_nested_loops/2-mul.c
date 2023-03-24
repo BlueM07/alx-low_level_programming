7c4f6c8a8b3bedf6c0135bd07c4292ee149f9154
Mariam <mariamernissi@gmail.com>
2023-03-24 14:52:49 -0700

    r

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @c: parameter
+(*
+* Description: multiplies two integers
+(*section header)
+* Return: result for multiplication for two integers
+*/
+int _isdigit(int a, int b)
+{
+c = (a * b);
+}
+return (%d, c);
+}
