295818c1cb9625ecd7df1a7df6d8cf01722ab230
Mariam <mariamernissi@gmail.com>
2023-03-24 15:26:25 -0700

    r

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *@i: parameter
+(*
+* Description: prin numbers from 0 to 9
+(*section header)
+* Return: line of numbers from 0 to 9
+*/
+void print_numbers(void)
+{
+for (i = 0; i>= 0 && i<+ 9; i++)
+{
+	_putchar(i);
+}
+return (0);
+}
