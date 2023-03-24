7066edb9796403bc62eff1f2cbbee811fd88e163
Mariam <mariamernissi@gmail.com>
2023-03-24 14:44:23 -0700

    r

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: parameter
+(*
+* Description: checks for uppercase character
+(*section header)
+* Return: 1 if uppser case, 0 if else
+*/
+int _isupper(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
