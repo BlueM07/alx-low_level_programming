50caff4d511dbf2ba250d4b09b13c34c425d2cb2
Mariam <mariamernissi@gmail.com>
2023-03-26 08:33:26 -0700

    r

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,6 +18,8 @@ void print_diagonal(int n)
                 for (i = 1; i <= n; i = i + 1)
                 {
                         _putchar('\');
+			_putchar('\n');
+			_putchar(' ');
                 }
                 _putchar('\n');
         }
