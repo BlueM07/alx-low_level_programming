ac9d6f357a67c65c14ee6f0c5e6264f8a1017adc
Mariam <mariamernissi@gmail.com>
2023-03-26 14:54:26 -0700

    r

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * islower - print _alphabet
+ * @c: parameter
+(*
+* Description: prints a specific string
+(* section header)
+* Return: the demanded string
+*/
+int _ islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar ('0');
+	}
+_putchar ('\n');
+}
