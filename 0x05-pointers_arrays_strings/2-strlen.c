91a8763fa985f67fcbbf64cdee8b844010a0cf77
Mariam <mariamernissi@gmail.com>
2023-03-27 12:29:55 -0700

    r

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - swaps the values of two integers
+ * @s:  parameter
+ * Description: swaps the values of two integers
+ * Return: values swaped
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = sizeof(*s);
+}
