366274938198c28c2f4cfe99f4a8c4cdc37c4626
Mariam <mariamernissi@gmail.com>
2023-03-27 13:55:45 -0700

    r

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - swaps the values of two integers
+ * @str: parameter
+ * Description: swaps the values of two integers
+ * Return: values swaped
+ */
+void _puts(char *str)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		putchar('len');
+		len++;
+	}
+putchar('\n');
+}
+
