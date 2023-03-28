fce932a2e17ac88c1e6644b0e34813d6da5c6bc3
Mariam <mariamernissi@gmail.com>
2023-03-28 10:36:01 -0700

    r

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - swaps the values of two integers
+ * @str: parameter
+ * Description: swaps the values of two integers
+ * Return: values swaped
+ */
+void puts_half(char *str)
+{
+	int len;
+	int n;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	n = (len - 1) / 2;
+	if (len >= n)
+	{
+		putchar(str[n]);
+		n++;
+	}
+	putchar('\n');
+}
