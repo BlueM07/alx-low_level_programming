fc81aff1c7703af0fdf54742a20ef0640e24403d
Mariam <mariamernissi@gmail.com>
2023-03-29 13:39:38 -0700

    r

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,26 @@
 #include "main.h"
 /**
- * _strcat - swaps the values of two integers
+ * _strncpy - swaps the values of two integers
  * @dest: parameter
  * @src: parameter
+ * @n: max used from src
  * Description: swaps the values of two integers
  * Return: values swaped
  */
-char *_strcat(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
-	char *strcat;
-
-	*strcat = dest + src;
+	int i = 0;
+	int j = 0;
+	
+	while (dest[i] != '\0')
+	{
+		dest[i] =src [j];
+		i++;
+		j++;
+	}
+	if (src[j] == \0)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
