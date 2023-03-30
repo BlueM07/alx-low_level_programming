ddc9d02a58b19e4f3549ba33705952b900907a49
Mariam <mariamernissi@gmail.com>
2023-03-30 15:37:51 -0700

    r

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,18 +11,24 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 65 && str[i] <= 90)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[i] = str[i] + 32;
-		}
-		if (str[i - 1] == 32 || str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46)
-		{
-			str[i] = str[i] - 32;
-		}
-		if (str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 || str[i - 1] == 40)
-		{
-			str[i] = str[i] - 32;
-		}
+			if (str[i - 1] == 32 || str[i - 1] == 44)
+			{
+				str[i] = str[i] - 32;
+			}
+			if (str[i - 1] == 59 || str[i - 1] == 46)
+			{
+				str[i] = str[i] - 32;
+			}
+			if (str[i - 1] == 33 || str[i - 1] == 63)
+			{
+				str[i] = str[i] - 32;
+			}
+			if (str[i - 1] == 34 || str[i - 1] == 40)
+			{
+				str[i] = str[i] - 32;
+			}
 		if (str[i - 1] == 41 || str[i - 1] == 123 || str[i - 1] == 125)
 		{
 			str[i] = str[i] - 32;
@@ -32,6 +38,7 @@ char *cap_string(char *str)
 			str[i] = str[i] - 32;
 		}
 		i++;
+		}
 	}
 	return (str);
 }
