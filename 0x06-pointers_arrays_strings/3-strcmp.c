004a2f29e8707ef09cac461327204556de36114a
Mariam <mariamernissi@gmail.com>
2023-03-30 09:42:47 -0700

    r

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,10 +14,14 @@ int _strcmp(char *s1, char *s2)
 	while ((s1[i] != '\0') && (s2[i] != '\0'))
 	{
 		strcmp = s1[i] - s2[i];
-			if (strcmp == 0)
-			{
-				i++;
-			}
+		if (strcmp == 0)
+		{
+			i++;
+		}
+		else if (strcmp != 0)
+				{
+					return (strcmp);
+				}
 	}
-	return (strcmp);
+return (0);
 }
