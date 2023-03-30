789bcd436091eac8332775bf41c83530dc97e8c2
Mariam <mariamernissi@gmail.com>
2023-03-30 15:53:29 -0700

    r

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,8 @@ char *cap_string(char *str)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
+			if (i == 0)
+				*str[i] -= 32
 			if (str[i - 1] == 32 || str[i - 1] == 44)
 			{
 				str[i] = str[i] - 32;
