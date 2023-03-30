d0c1a6858a32e06996a8c580e1b6efe14c23ee86
Mariam <mariamernissi@gmail.com>
2023-03-30 15:28:16 -0700

    r

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,21 +15,21 @@ char *cap_string(char *str)
 		{
 			str[i] = str[i] + 32;
 		}
-		if (str[i] == 32 || str[i] == 44 || str[i] == 59 || str[i] == 46)
+		if (str[i - 1] == 32 || str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46)
 		{
-			str[i + 1] = str[i + 1] - 32;
+			str[i] = str[i] - 32;
 		}
-		if (str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40)
+		if (str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 || str[i - 1] == 40)
 		{
-			str[i + 1] = str[i + 1] - 32;
+			str[i] = str[i] - 32;
 		}
-		if (str[i] == 41 || str[i] == 123 || str[i] == 125)
+		if (str[i - 1] == 41 || str[i - 1] == 123 || str[i - 1] == 125)
 		{
-			str[i + 1] = str[i + 1] - 32;
+			str[i] = str[i] - 32;
 		}
-		if (str[i] == 9 || str[i] == 10)
+		if (str[i - 1] == 9 || str[i - 1] == 10)
 		{
-			str[i + 1] = str[i + 1] - 32;
+			str[i] = str[i] - 32;
 		}
 		i++;
 	}
