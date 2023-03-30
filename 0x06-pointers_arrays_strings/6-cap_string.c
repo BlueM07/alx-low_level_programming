a6aabd96c49089b5891f756ec2ff3473e61ca6fc
Mariam <mariamernissi@gmail.com>
2023-03-30 14:53:53 -0700

    r

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,19 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == 32)
+		if (str[i] == 32 || str[i] == 44 || str[i] == 59 || str[i] == 46)
+		{
+			str[i] + 1 = str[i] - 31;
+		}
+		if (str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40)
+		{
+			str[i] + 1 = str[i] - 31;
+		}
+		if (str[i] == 41 || str[i] == 123 || str[i] == 125)
+		{
+			str[i] + 1 = str[i] - 31;
+		}
+		if (str[i] == 9 || str[i] == 10)
 		{
 			str[i] + 1 = str[i] - 31;
 		}
