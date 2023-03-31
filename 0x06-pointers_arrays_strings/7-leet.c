d83cf222bcb8a401b0a98c9dbac560fe9c9c9a56
Mariam <mariamernissi@gmail.com>
2023-03-31 15:41:52 -0700

    r

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,23 @@
 #include "main.h"
 /**
- * _strcat - swaps the values of two integers
- * @dest: parameter
- * @src: parameter
+ * leet - swaps the values of two integers
+ * @str: parameter
  * Description: swaps the values of two integers
  * Return: values swaped
  */
-char *_strcat(char *dest, char *src)
+char *leet(char *str)
 {
-	char *strcat;
-
-	*strcat = dest + src;
+	int i;
+	int j = 0;
+	char a[] = "'a''A''e''E''o''O''t''T''l''L'\0";
+	int b[] = {4,4,3,3,0,0,7,7,1,1};
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == a[j])
+		{
+			str[i] = b[j];
+		}
+	}
+	return (0);
 }
