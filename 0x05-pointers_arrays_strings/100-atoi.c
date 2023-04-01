9726d01a07f83b170100150e7eea364725b28721
Mariam <mariamernissi@gmail.com>
2023-04-01 12:54:25 -0700

    r

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,11 +8,18 @@
 int _atoi(char *s)
 {
 	int i;
+	int j;
+	char a[] = "0123456789";
+	int b[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 48 && s[i] <= 57)
+		for (j = 0; a[j] != '\0'; j++)
 		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
 			return (s[i]);
 		}
 	}
