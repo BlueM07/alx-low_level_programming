0d4693cdbec2956a0cfcdc0431573d767582593b
Mariam <mariamernissi@gmail.com>
2023-04-01 10:13:20 -0700

    r

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -19,7 +19,7 @@ void rev_string(char *str)
 	for (i = 0; i < ((len - 1) / 2); i++)
 	{
 		temp = str[i];
-		str[i] = str[len - i];
-		str[len - i] = temp;
+		str[i] = str[len - 1 - i];
+		str[len - 1 - i] = temp;
 	}
 }
