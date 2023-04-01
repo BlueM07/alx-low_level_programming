5f3e5f6ad966b514077e2e72ee31581954d09e0f
Mariam <mariamernissi@gmail.com>
2023-04-01 09:27:42 -0700

    r

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_number - swaps the values of two integers
+ * @n: parameter
+ * Description: swaps the values of two integers
+ * Return: values swaped
+ */
+int print_number(int n)
+{
+	putchar(n);
+return (0);
+}
+
