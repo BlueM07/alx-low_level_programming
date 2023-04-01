50944e851cfd9b99c8ce69927edcaaaad36dbb05
Mariam <mariamernissi@gmail.com>
2023-04-01 14:59:52 -0700

    r

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,17 +10,25 @@ int _atoi(char *s)
 	int i;
 	int res = 0;
 	int neg = 1;
+	int isi = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		if (s[i - 1] == '-')
+		if (s[i] == '-')
 		{
 			neg = -1;
 		}
-		if (s[i] >= 48 && s[i] <= 57)
+		while (s[i] >= 48 && s[i] <= 57)
 		{
+			isi = 1;
 			res = res * 10 + s[i] - 48;
+			i++;
 		}
+		if (isi == 1)
+		{
+			break;
+		}
+		i++;
 	}
 res = res *neg;
 return (res);
