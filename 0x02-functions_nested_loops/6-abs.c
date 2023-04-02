267456864c8fc5367d3663cf1da47bd7594cf01c
Mariam <mariamernissi@gmail.com>
2023-04-02 10:07:06 -0700

    r

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_abs - print _alphabet
+ * @n: parameter
+(*
+* Description: prints a specific string
+(* section header)
+* Return: the demanded string
+*/
+int _abs(int n)
+{
+	int r = 0;
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	r = n;
+return (n);
+}
