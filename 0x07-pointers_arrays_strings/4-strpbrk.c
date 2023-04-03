44b23c37cc5d509d0427ca18906bd8e2fb75f937
Mariam <mariamernissi@gmail.com>
2023-04-03 16:28:16 -0700

    r

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,22 +13,19 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] != 32)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
 			{
-				if (s[i] == accept[j])
-				{
-					return (s);
-				}
+				break;
 			}
 		}
-		else
+		if (s[i] == accept[j])
 		{
-			return (NULL);
+			break;
 		}
 	}
-	return (NULL);
+	return (s + i);
 }
 
 
