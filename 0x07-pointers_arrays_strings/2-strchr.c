9f38a583a1645bb4a61288d8db4cb337e6408688
Mariam <mariamernissi@gmail.com>
2023-04-03 12:59:53 -0700

    r

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-		if (c == '\0')
+		if (*s == c)
 		{
 			return (s);
+		}
+		s++;
+		if (c == '\0')
+		{
+			return (NULL);
 		}
 	}
 return (NULL);
