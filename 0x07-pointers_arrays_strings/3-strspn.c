4e1173bd81610b1b5266c279d7d314414b98be34
Mariam <mariamernissi@gmail.com>
2023-04-03 13:30:50 -0700

    r

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *  _strspn - function to set memory
+ * @s: string
+ * @accept: char
+ * Description: function to set memory with a constant byte
+ * Return: pointer to the memory area
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		if (*s == *accept)
+		{
+			len = len + 1;
+		}
+		s++;
+	}
+	return (len);
+}
+
+
