b12d298d15d2cd52616765f6a50eaee9405ee208
Mariam <mariamernissi@gmail.com>
2023-04-03 09:41:23 -0700

    r

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  _memcpy - function to set memory
+ * @dest: string
+ * @src: constant byte
+ * @n: int
+ * Description: function to set memory with a constant byte
+ * Return: pointer to the memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			dest[i] = src[j];
+		}
+	}
+	return (dest);
+}
