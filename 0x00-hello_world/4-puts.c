d1e6e884ba20f933e6c247f58a34c034274c86e0
BlueM07 <mariamernissi@gmail.com>
2023-09-07 09:40:29 -0700

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main: function that puts and returns a string
+ * main - function that puts and returns a string
  *
  * Description: Longer description of the function
  * Return: The wanted result
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
