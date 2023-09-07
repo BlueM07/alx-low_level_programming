6d8dcdf987b0427a3cb54db3a2f54d40bbae3c6f
BlueM07 <mariamernissi@gmail.com>
2023-09-07 09:50:54 -0700

    r

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function that puts and returns a string
+ *
+ * Description: Longer description of the function
+ * Return: The wanted result
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
