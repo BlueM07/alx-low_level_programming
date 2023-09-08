ffd30ab68171828b03b88fc1923e655a9d183005
BlueM07 <mariamernissi@gmail.com>
2023-09-08 13:05:22 -0700

    r

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function that puts and returns a string
+ *
+ * Description: Longer description of the function
+ * Return: The wanted result
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar (a);
+	}
+	putchar ('\n');
+	return (0);
+}
