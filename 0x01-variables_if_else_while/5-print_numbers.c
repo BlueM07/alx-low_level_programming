26ce143c1bcd20d6fb623283e167f715d7931b3e
BlueM07 <mariamernissi@gmail.com>
2023-09-08 13:00:28 -0700

    r

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function that puts and returns a string
+ *
+ * Description: Longer description of the function
+ * Return: The wanted result
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9' ; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
+}
