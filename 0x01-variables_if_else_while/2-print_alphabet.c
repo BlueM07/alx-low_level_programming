9744c07b349ec2d597b7f47205c21ef82b247355
BlueM07 <mariamernissi@gmail.com>
2023-09-08 11:23:29 -0700

    r

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - function that puts and returns a string
+ *
+ * Description: Longer description of the function
+ * Return: The wanted result
+ */
+int main(void)
+{
+	int i;
+	char a;
+
+	a = 97;
+	for (i = 0; a <= 122 ; a++)
+	{
+		putchar (a);
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
