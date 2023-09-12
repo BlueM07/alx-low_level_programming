3b34d53f3f8bc3e846521eb7fafeba82dcbe668c
BlueM07 <mariamernissi@gmail.com>
2023-09-12 15:22:17 -0700

    r

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,14 @@
 void print_alphabet(void)
 {
 	char a;
+	int i = 0;
 
-	for (a = 'a'; a <= 'z'; a++)
+	for (i = 0; i <= 10; i++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
-			for (a = 'a'; a <= 'z'; a++)
-			{
-				putchar(a);
-			}
+			putchar(a);
 		}
+		putchar ('\n');
 	}
-	putchar ('\n');
 }
