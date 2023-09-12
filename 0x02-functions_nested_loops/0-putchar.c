dcaceaa995cd069aea331206adf5438ede98fb5f
BlueM07 <mariamernissi@gmail.com>
2023-09-12 11:42:18 -0700

    r

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - function that puts and returns a string
+ *
+ * Description: Longer description of the function
+ * Return: The wanted result
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	putchar ('\n');
+	return (0);
+}
