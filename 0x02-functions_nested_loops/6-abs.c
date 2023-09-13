7ff432c12e8431e5820af635f0b6ea8f7286ce89
BlueM07 <mariamernissi@gmail.com>
2023-09-13 14:48:57 -0700

    r

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * abs - function that puts and returns a string
+ * @n: parameter
  *
  * Description: Longer description of the function
  * Return: The wanted result
  */
-int _abs(int)
+int _abs(int n)
 {
-	int n;
-
 	if (n >= 0)
 	{
 		n = n;
