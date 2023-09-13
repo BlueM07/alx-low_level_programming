82781688148c1a0a5c7c318b0fddff1b53b0705b
BlueM07 <mariamernissi@gmail.com>
2023-09-13 13:51:55 -0700

    r

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * _islower - function that puts and returns a string
+ * @c: conditional parameter
  *
- * c : conditional paramtere
  * Description: Longer description of the function
  * Return: The wanted result
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		{
-			return (1);
-		}
+	{
+		return (1);
+	}
 	else
-		{
-			return (0);
-		}
+	{
+		return (0);
+	}
 }
