be9a2866eab7229022c185d6bac84d4f45da8f85
BlueM07 <mariamernissi@gmail.com>
2023-09-13 15:23:50 -0700

    r

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - function that puts and returns a string
+ * @n: parameter
+ *
+ * Description: Longer description of the function
+ * Return: The wanted result
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		n = n;
+	}
+	else if (n < 0)
+	{
+		n = -n;
+	}
+	return (n);
+}
