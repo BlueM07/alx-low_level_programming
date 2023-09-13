cfa8e40a5c0bdd6a6170d8b4f7032d8c10255303
BlueM07 <mariamernissi@gmail.com>
2023-09-13 15:09:32 -0700

    r

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - function that puts and returns a string
+ * @n: parameter
+ *
+ * Description: Longer description of the function
+ * Return: The wanted result
+ */
+int print_last_digit(int n)
+{	
+	n = n % 10;
+	return (n);
+}
