359699ad06e81d152c206ee4d488f2308773ef36
BlueM07 <mariamernissi@gmail.com>
2023-09-14 14:00:09 -0700

    r

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - decription of the function
+ * @c: parameter
+ * Return: function result
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		putchar(1);
+	}
+	else
+	{
+		putchar(0);
+	}
+	return (0);
+}
+
