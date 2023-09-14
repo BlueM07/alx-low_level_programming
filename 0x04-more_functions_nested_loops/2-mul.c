7a22ff6b7afa180d61917a46abd185b84097ddde
BlueM07 <mariamernissi@gmail.com>
2023-09-14 14:51:23 -0700

    r

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - decription of the function
+ * @a: parameter
+ * @b: parameter
+ * Return: function result
+ */
+int mul(int a, int b)
+{
+	int l = a * b;
+
+	_putchar(l);
+	return (0);
+}
