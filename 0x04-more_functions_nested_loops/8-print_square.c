bbd2e63e2842a6cb5eaf5e9a26349f75db951f57
BlueM07 <mariamernissi@gmail.com>
2023-09-15 14:23:11 -0700

    r

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- *print_diagonal - decription of the function
- *@n: parameter
+ * print_square - decription of the function
+ * @size: parameter
  * Return: function result
  */
-void print_diagonal(int n)
+void print_square(int size)
 {
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	if (size <= 0)
 	{
-		if (n > 0)
-	{
-		_putchar('\\');
 		_putchar('\n');
-		for (j = 0; j <= i; j++)
+	}
+	else
+	{
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
 		{
-			_putchar(32);
+			_putchar('#');
 		}
+		_putchar('\n');
 	}
 	}
-	_putchar('\n');
 }
