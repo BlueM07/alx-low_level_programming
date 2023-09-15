95f73a56d6e5d758649564531cd2de532a6129c3
BlueM07 <mariamernissi@gmail.com>
2023-09-15 14:00:24 -0700

    r

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,13 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; i <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i > 9)
+			if (j > 9)
 			{
 				_putchar('1');
 			}
-		_putchar('0' + i % 10)
+		_putchar('0' + j % 10)
 		}
 	}
 		_putchar('\n');
