b751594957d9cbde4d82681e050e38f2e9c770fe
BlueM07 <mariamernissi@gmail.com>
2023-09-18 15:12:32 -0700

    r

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - decription of the function
+ * @s: parameter
+ * Return: function result
+ */
+int _strlen(char *s)
+{
+	int len;
+	int i;
+	char s[n];
+
+	for (i = 0; i < n; i++)
+	{
+		len = i;
+	}
+}
