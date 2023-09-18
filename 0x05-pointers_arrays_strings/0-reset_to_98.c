21246236276e7d2ef3f21bba0bbcb102cce235ca
BlueM07 <mariamernissi@gmail.com>
2023-09-18 11:20:28 -0700

    r

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - decription of the function
+ * @n: parameter
+ * Return: function result
+ */
+void reset_to_98(int *n)
+{
+	int c;
+
+	n = &c;
+	*n = 98;
+}
