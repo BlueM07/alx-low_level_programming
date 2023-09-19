32f56855fe785a822a990d493c362dae7369607f
BlueM07 <mariamernissi@gmail.com>
2023-09-18 23:57:43 -0700

    r

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - decription of the function
+ * @s: parameter
+ * Return: function result
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j = 0;
+	int len = 0;
+	char stemp[1000];
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = i + 1;
+	}
+	for (i = len - 1; i >= 0; i--, j++)
+	{
+		stemp[j] = s[i];
+	}
+	stemp[len] = '\0';
+	for (i = 0, j = 0; s[i] != '\0'; i++, j++)
+	{
+		s[i] = stemp[j];
+	}
+}
