30dcd1b9b70dd807f060d1a828f47b61584938b7
BlueM07 <mariamernissi@gmail.com>
2023-09-19 12:01:12 -0700

    r

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,12 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
-	
+	int n;
+
 	for (i = 0; i < (n - 1); i++)
-		{	
-			printf("%d, ", a[i]);
-		}
+	{
+		printf("%d, ", a[i]);
+	}
 	printf("%d", a[(n - 1)]);
 	printf("\n");
 }
