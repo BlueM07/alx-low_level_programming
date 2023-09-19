d9742240a84fade8decc899495ce9a9e0c65ce8d
BlueM07 <mariamernissi@gmail.com>
2023-09-18 23:47:05 -0700

    r

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,10 @@
 void puts2(char *str)
 {
 	int i;
-	int len = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i = i + 2)
 	{
-		len = i + 1;
-	}
-	for (i = 0; s[i] != '\0'; i = i + 2)
-	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
