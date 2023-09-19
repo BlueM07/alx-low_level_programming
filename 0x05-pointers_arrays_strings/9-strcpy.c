4b5ac6d1f13c6f09061adfabc9bf8ef109a81abf
BlueM07 <mariamernissi@gmail.com>
2023-09-19 13:53:41 -0700

    r

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_strcpy - decription of the function
+ * @dest: parameter
+ * @src: parameter
+ * Return: function result
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; dest[i] != '\0' && src[i] != '\0'; i++)
+	{
+		*dest[i] = *src[i];
+	}
+	return (*dest);
+}
