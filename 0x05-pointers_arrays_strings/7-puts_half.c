a5bb511c67d994a8404914ae3fd131bc5d6486e3
BlueM07 <mariamernissi@gmail.com>
2023-09-19 10:57:40 -0700

    r

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,14 +16,14 @@ void puts_half(char *str)
 	}
 	if (len % 2 == 0)
 	{
-		for (i = (len - 1) / 2; str[i] != '\0'; i++)
+		for (i = len / 2; str[i] != '\0'; i++)
 		{
 		_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = len / 2; str[i] != '\0'; i++)
+		for (i = (len - 1) / 2; str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
