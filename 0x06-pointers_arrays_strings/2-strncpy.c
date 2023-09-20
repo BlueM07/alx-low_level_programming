2ca10bf925723570c65c5949f7c8d345305c0588
BlueM07 <mariamernissi@gmail.com>
2023-09-20 10:41:52 -0700

    r

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,36 +10,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
-	int ldest = 0;
-	int lsrc = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		ldest = i + 1;
-	}
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		lsrc = i + 1;
+		dest[i] = src[i];
 	}
-	if (ldest <= lsrc)
+	for (i = i; i < n; i++)
 	{
-		for (i = 0, j = 0; j < n && src[j] != '\0'; i++, j++)
-		{
-			dest[i] = src[j];
-		}
+		dest[i] = '\0';
 	}
-	else
-	{
-		for (i = 0; dest[i] != '\0'; i++)
-		{
-			dest[i] = '\0';
-		}
-		for (i = 0, j = 0; j < n && src[j] != '\0'; i++, j++)
-		{
-			dest[i] = src[j];
-		}
-	}
-	dest[i] = '\0';
 	return (dest);
 }
