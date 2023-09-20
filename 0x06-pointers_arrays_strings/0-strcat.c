61d27513fac15f92effdf61b51f1e423a672abde
BlueM07 <mariamernissi@gmail.com>
2023-09-20 09:42:47 -0700

    r

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,13 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
+	int len = 0;
 
 	for (i = 0; dest[i] != '\0'; i++)
+	{
+		len = i;
+	}
+	for (i = len; src[i] != '\0'; i++)
 	{
 		for (j = 0; src[j] != '\0'; j++)
 		{
