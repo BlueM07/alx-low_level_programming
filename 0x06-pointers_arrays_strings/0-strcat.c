d4b0eb4abdf05cf77a0b45b2701fbda776fe7e70
BlueM07 <mariamernissi@gmail.com>
2023-09-20 09:35:04 -0700

    r

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - decription of the function
+ * @dest: parameter
+ * @src: parameter
+ * Return: function result
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		for (j = 0; src[j] != '\0'; j++)
+		{
+			dest[i] = src[j];
+		}
+	}
+	dest[j] = '\0';
+}
