cf7c5a785e8c466cce0ffb1f24d9c91e0c6a679e
BlueM07 <mariamernissi@gmail.com>
2023-09-20 12:16:46 -0700

    r

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,36 +9,15 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int j;
-	int ls1;
-	int ls2;
 	int cmp = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		ls1 = i + 1;
-	}
-	for (i = 0; s2[i] != '\0'; i++)
-	{
-		ls2 = i + 1;
-	}
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		cmp = s1[i] - s2[i];
 		i++;
-	}
-	if (ls1 > ls2)
-	{
-		for (j = 0; s1[j] != '\0'; j++)
+		if (cmp != 0)
 		{
-			cmp = cmp + s1[j];
-		}
-	}
-	else if (ls1 < ls2)
-	{
-		for (j = 0; s2[j] != '\0'; j++)
-		{
-			cmp = cmp - s2[j];
+			return (cmp);
 		}
 	}
 	return (cmp);
