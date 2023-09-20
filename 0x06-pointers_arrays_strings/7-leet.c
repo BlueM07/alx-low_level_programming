1ef118f172135076b16204a7818be6ab5f81c828
BlueM07 <mariamernissi@gmail.com>
2023-09-20 14:00:35 -0700

    r

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,15 +8,20 @@
 char *leet(char *s)
 {
 	int i;
+	int j;
+
 	char alow[5] = {'a', 'e', 'o', 't', 'l'};
 	char aupper[5] = {'A', 'E', 'O', 'T', 'L'};
 	int replace[5] = {4, 3, 0, 7, 1};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == alow[i] || s[i] == aupper[i])
+		for (j = 0; alow[j] != '\n' && aupper[j] != '\n' && replace[j] != '\n')
 		{
-			s[i] = replace[i];
+			if (s[i] == alow[i] || s[i] == aupper[i])
+			{
+				s[i] = replace[i];
+			}
 		}
 	}
 	return (s);
