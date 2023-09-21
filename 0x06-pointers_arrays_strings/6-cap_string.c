e7ea28936c48e729103e2bcede1b552d653c4e21
BlueM07 <mariamernissi@gmail.com>
2023-09-21 09:48:00 -0700

    r

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,35 @@
 #include "main.h"
 
 /**
- * *string_toupper - decription of the function
+ * *cap_string - decription of the function
  * @s: parameter
  * Return: function result
  */
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (s[i] == 32 || s[i] == 9 || s[i] == 10)
 		{
-			s[i] = s[i] - 32;
+			s[i + 1] = s[i + 1] - 32;
+		}
+		if (s[i] == 44 || s[i] == 59 || s[i] == 46)
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+		if (s[i] == 44 || s[i] == 59 || s[i] == 46)
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+		if (s[i] == 33 || s[i] == 63 || s[i] == 34 || s[i] == 40)
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+		if (s[i] == 41 || s[i] == 123 || s[i] == 125)
+		{
+			s[i + 1] = s[i + 1] - 32;
 		}
 	}
 	return (s);
