f53fc86399f2678277f5d470cb3299d02bf637fc
BlueM07 <mariamernissi@gmail.com>
2023-09-24 11:13:44 -0700

    r

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "limits.h"
 
 /**
  * print_last_digit - function that puts and returns a string
@@ -10,8 +11,15 @@
 int print_last_digit(int n)
 {
 	int mod;
+	int min;
 
+	min = INT_MIN;
 	mod = n % 10;
+	if (n == min)
+	{
+		_putchar (8);
+		return (8);
+	}
 	if (n >= 0)
 	{
 		_putchar ('0' + mod);
