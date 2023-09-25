8afbda4a0377b60079c6346e787d9a18cb99538b
BlueM07 <mariamernissi@gmail.com>
2023-09-25 13:54:18 -0700

    r

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,16 +17,16 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[0])
 		{
 			ref = i;
-			for (j = 0; needle[j] != '\0'; j++)
+		}
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] != needle[j])
 			{
-				if (haystack[i] != needle[j])
-				{
-					return (NULL);
-				}
-				else
-				{
-					return (&haystack[ref]);
-				}
+				return (NULL);
+			}
+			else
+			{
+				return (&haystack[ref]);
 			}
 		}
 	}
