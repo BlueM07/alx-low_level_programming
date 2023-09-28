09ff1951c47fe1ee9b4f8c423464339f1fdf581c
BlueM07 <mariamernissi@gmail.com>
2023-09-28 13:13:30 -0700

    r

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - decription of the function
+ * @s: parameter
+ * Return: function result
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return(0);
+	}
+	return (l + _strlen_recursion(s + 1));
+}
