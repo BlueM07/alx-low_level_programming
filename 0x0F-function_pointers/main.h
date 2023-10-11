092a932ab15b1d09a2b092d2e34e911f67f07f10
BlueM07 <mariamernissi@gmail.com>
2023-10-11 14:48:17 -0700

    r

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(int c);
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
+
+#endif /* MAIN_H */
