740e877fd948bbb61644acb18b57d39fee155c14
BlueM07 <mariamernissi@gmail.com>
2023-10-19 12:43:27 -0700

    r

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,14 +9,14 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t total;
+size_t total;
 
-    total = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        total++;
-    }
-    return (total);
+total = 0;
+while (h != NULL)
+{
+h = h->next;
+total++;
+}
+return (total);
 
 }
