86bdedc94d77bb3719d36dc7a9ce04c1b7df3b62
BlueM07 <mariamernissi@gmail.com>
2023-10-29 11:57:37 -0700

    r

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * binary_to_uint - permits the conversion of a binary to uint
+ * @b: parameter
+ * Return: rturn
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	int len;
+	unsigned int result = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		len = len + i;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			result = b[i] + 2 * (len - i);
+		}
+	}
+	return (result);
+}
+
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,4 @@
+#include<stdio.h>
+
+int _putchar();
+unsigned int binary_to_uint(const char *b);
