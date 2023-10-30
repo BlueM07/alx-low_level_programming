1fbea6973b1ff0e1e2a2f52a5f248943d4e637fa
BlueM07 <mariamernissi@gmail.com>
2023-10-30 11:08:02 -0700

    r

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,19 @@ unsigned int binary_to_uint(const char *b)
 	int j = 1;
 	unsigned int result = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == '\0' || (b[i] != '0' && b[i] != '1'))
+		if ((b[i] != '0' && b[i] != '1'))
 		{
 			return (0);
 		}
 		len++;
 	}
-	for (i = len; i > 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] == '1')
 		{
