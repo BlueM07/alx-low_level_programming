af86d316e218dc101ef8274815ea39d08b28a3dd
BlueM07 <mariamernissi@gmail.com>
2023-11-13 12:48:09 -0800

    r

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,36 @@
 #include "main.h"
 
+
+
+/**
+ * _length - length
+ *
+ * @n: value
+ *
+ * Return: length
+*/
+int _length(unsigned long int n)
+{
+	int length = 0;
+
+	while (n > 0)
+	{
+		
+
+		length++;
+		n >>= 1; /*shift n to the right by 1*/
+
+		
+	}
+
+	length--;
+
+	return (length);
+}
+
+
+
+
 /**
  * print_binary - a function 
  *
